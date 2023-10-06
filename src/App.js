e04244dc9670e813cd4a8608b2d16898a5ebe566
Devesh <deveshamway@gmail.com>
2023-10-07 00:36:41 +0530

    Basic design done and form is working

--- src/App.js
+++ src/App.js
@@ -1,23 +1,93 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import { useState } from 'react';
+
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: true },
+  { id: 2, description: "Charger", quantity: 2, packed: false },
+];
+
+function Logo() {
+  return <h1>🏝️ Pack Pal 🧳</h1>
+}
+
+function Form() {
+  const [description, setDescription] = useState("");
+  const [quantity, setQuantity] = useState(1);
+
+  function handleSubmit(e) {
+    e.preventDefault();    // prevent page reloading
+
+    if(!description) return;
+
+    const newItem = {description, quantity, packed:false, id:Date.now()};
+    
+    setDescription("");
+    setQuantity(1);
+  }
+
+  return (
+    <form className="add-form" onSubmit={handleSubmit}>
+      <h3>What do you need for your 😍 trip?</h3>
+      <select 
+        value={quantity}
+        onChange={(e)=>setQuantity(Number(e.target.value))}
+      >
+        {Array.from({length:20}, (_,i)=>i+1).map
+        ((num)=>(
+          <option value = {num} key={num}>
+            {num}
+          </option>
+        ))}
+      </select>
+      <input 
+        type="text" 
+        placeholder="Item..." 
+        value={description} 
+        onChange={(e) => setDescription(e.target.value)}
+      />
+      <button>Add</button>
+    </form>
+  );
+}
+
+function PackingList() {
+  return (
+    <div className="list">
+      <ul >
+        {initialItems.map((item)=>(
+          <Item item={item} key={item.id} />
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+function Item({item}){
+  return <li>
+    <span style={item.packed ? {textDecoration : "line-through"} : {}}>
+      {item.quantity} {item.description}
+    </span>
+    <button>❌</button>
+  </li>
+}
+
+function Stats(){
+  return (
+    <footer className="stats">
+      <em>💼 You have X items on your list, and you already packed X (X%)</em>
+    </footer>
+  );
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Logo/>
+      <Form/>
+      <PackingList/>
+      <Stats/>
     </div>
   );
 }
