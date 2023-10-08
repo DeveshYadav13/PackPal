faa3e527db1e2912032ae61ecb42a6493ef720a2
Devesh <deveshamway@gmail.com>
2023-10-09 00:36:16 +0530

    Added stats % component's code

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import { useState } from 'react';
 
 function App() {
   const [items, setItems] = useState([]);
-  
+
   function handleAddItems(item){
     setItems(items=>[...items, item]);
   }
@@ -29,7 +29,7 @@ function App() {
       <Logo/>
       <Form onAddItems={handleAddItems} />
       <PackingList items={items} onDeleteItem={handleDeleteItem} onToggleItem={handleToggleItem}/>
-      <Stats/>
+      <Stats items={items} />
     </div>
   );
 }
@@ -106,10 +106,26 @@ function Item({item, onDeleteItem, onToggleItem}){
   </li>
 }
 
-function Stats(){
+function Stats({items}){
+  if(!items.length){
+    return <p className="stats">
+      <em>
+        Start adding some items to your packing list..
+      </em>
+    </p>
+  }
+
+  const numItems = items.length;
+  const numPacked = items.filter(item=>item.packed).length;
+  const percentage = Math.round((numPacked/numItems)*100);
+  
   return (
     <footer className="stats">
-      <em>💼 You have X items on your list, and you already packed X (X%)</em>
+      <em>
+        {percentage===100 ? 'You got everything! Ready to go ' : 
+        `💼 You have ${numItems} items on your list, and you already packed ${numPacked} (${percentage}%)`
+      }
+        </em>
     </footer>
   );
 }
